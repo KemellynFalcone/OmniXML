7d8a0b4593dc7019c13da9c4d36f04ae18bdb16c
KEMELLYN CRISTINA FALCONE <suportefalcone2018@gmail.com>
2026-08-27 16:19:12 -0300

    refactor: isolar servidor web do backend legado

--- web_app_browser.py
+++ web_app_browser.py
@@ -1,14 +1,20 @@
-from flask import Response, render_template
+from flask import Flask, Response, jsonify, render_template
 
-import web_app as legacy
-
-app = legacy.app
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
-def index_browser_local():
+@app.get('/')
+def index():
     html = render_template('dashboard.html')
     ponte = '<script src="/static/browser_local.js?v=1"></script>'
     return Response(html.replace('</body>', f'{ponte}</body>'), mimetype='text/html')
 
 
-app.view_functions['index'] = index_browser_local
+@app.get('/health')
+def health():
+    return jsonify({
+        'status': 'ok',
+        'service': 'OmniXML Web',
+        'processing': 'browser-local',
+        'xml_upload': False,
+    })
