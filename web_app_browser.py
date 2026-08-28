0f7b143f2d045f36ec6d18f63ffd91c10382ef54
KEMELLYN CRISTINA FALCONE <suportefalcone2018@gmail.com>
2026-08-28 16:44:25 -0300

    ux: carregar diagnóstico compacto v2

--- web_app_browser.py
+++ web_app_browser.py
@@ -10,7 +10,7 @@ def index():
         '<script src="/static/failure_table_v2.js?v=2"></script>'
         '<script src="/static/browser_validation.js?v=1"></script>'
         '<script src="/static/failure_summary.js?v=1"></script>'
-        '<script src="/static/closing_diagnosis.js?v=1"></script>'
+        '<script src="/static/closing_diagnosis_v2.js?v=2"></script>'
         '<script src="/static/browser_local_v2.js?v=2"></script>'
     )
     return Response(html.replace('</body>', f'{ponte}</body>'), mimetype='text/html')
@@ -26,6 +26,6 @@ def health():
         'fiscal_validation': 'authorization-structure',
         'failure_summary': 'unique-files',
         'failure_table': 'v2-key-value-reason',
-        'closing_diagnosis': 'sequence-gaps-and-pendencies',
+        'closing_diagnosis': 'compact-conservative-v2',
         'xml_upload': False,
     })
